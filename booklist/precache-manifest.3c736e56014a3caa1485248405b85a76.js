bd077b52b83f7dd8a149851bc5279f29a65de00f
kopchikovich <kopchikovich@yandex.ru>
2020-01-09 08:33:17 +1000

    Add booklist template

---
+++ booklist/precache-manifest.3c736e56014a3caa1485248405b85a76.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce8391a9b40257d4a46a88a9f7170a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2225279f677b3faecc3e",
+    "url": "/static/js/2.49033ad5.chunk.js"
+  },
+  {
+    "revision": "19736281eeb3bed28555",
+    "url": "/static/js/main.795a6c7d.chunk.js"
+  },
+  {
+    "revision": "145b952be6fa5c1472c8",
+    "url": "/static/js/runtime-main.e6065773.js"
+  }
+]);
