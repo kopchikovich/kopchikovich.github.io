c211c0ac4b17e87bb6d7a48892a9936401fb5686
kopchikovich <kopchikovich@yandex.ru>
2020-01-09 08:30:07 +1000

    Clear repo

--- source/js/main.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".btn-start").onclick = function() {
-  document.querySelector(".app").innerHTML = '<div class="app__train-page"><h2>Choose workout</h2><ul><li>Run workout 1</li><li>Run workout 2</li><li>Run workout 3</li><li>Power workout 1</li><li>Power workout 2</li><li>Power workout 3</li></ul></div>';
-};
