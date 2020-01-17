08470effc1db0d5913c0eee2fa977aed75b66bd0
kopchikovich <kopchikovich@yandex.ru>
2020-01-18 08:40:09 +1000

    Add calc-maker

---
+++ calc-maker/precache-manifest.648a2450457576b3a1c65475182e2d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6eaa00445c038843f0c990b583a69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0667a507eae28c6042cc",
+    "url": "/static/css/main.907a2d42.chunk.css"
+  },
+  {
+    "revision": "729ba5114721201d5bc7",
+    "url": "/static/js/2.8b40d3b4.chunk.js"
+  },
+  {
+    "revision": "0667a507eae28c6042cc",
+    "url": "/static/js/main.532f87e7.chunk.js"
+  },
+  {
+    "revision": "bfa52da06666883a2775",
+    "url": "/static/js/runtime-main.c88d2822.js"
+  }
+]);
